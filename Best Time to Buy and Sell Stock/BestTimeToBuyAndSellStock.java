31edbf218c2d8068c9b76b17f9a4edf9066c99bd
FilipS90 <filip.stojiljkovic90@yahoo.com>
2023-02-17 20:11:30 +0100

    better result for stock problem

--- Best Time to Buy and Sell Stock/BestTimeToBuyAndSellStock.java
+++ Best Time to Buy and Sell Stock/BestTimeToBuyAndSellStock.java
@@ -1,28 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length,min=Integer.MAX_VALUE,max=Integer.MIN_VALUE,q1=0,q2=0,output=0;
-	    boolean provera1=false, provera2=false;
-	     
-	        for(int i=0;i<n;i++){
-	            if(prices[i]<min){
-	                if(i+1==n)
-	                    break;
-	                else{
-	                min=prices[i];
-	                q1=i;
-	                provera1=true;
-                    	continue;
-	                }
-	            }
-	            if(prices[i]>min && q1<i){
-	                max=prices[i];
-	                q2=i;
-	                provera2=true;
-	            }
-	            if(max-min>output && provera1==true && provera2==true && q1<q2)
-	                output=max-min;
-	        }
-	        return output;
-        
+        if(prices.length == 1)
+            return 0;
+
+        int max = 0, profit = 0;
+
+        for (int i = prices.length - 1; i >= 0; i--){
+            if(max <= prices[i]){
+                max = prices[i];
+                continue;
+            } else {
+                profit = (max - prices[i]) > profit ? max - prices[i]: profit;
+            }
+        }
+
+        return profit;
     }
 }
